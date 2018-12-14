29344777c21488f3c89feff943f75063939f7390
tori25 <vera.mezhvynskiy@gmail.com>
2018-12-14 15:07:43 +0200

    removed sql from git

--- app/sql/local.wp_postmeta.sql
+++
@@ -1,9 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_postmeta` VALUES
-(1,2,"_wp_page_template","default"),
-(2,3,"_wp_page_template","default"),
-(3,5,"_edit_lock","1544615870:1"),
-(6,8,"_edit_lock","1544615807:1"),
-(15,12,"_edit_lock","1544617901:1");
