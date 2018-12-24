4f1f2ecc4eaf524be03cc643ae7edbd204f15e02
tori25 <vera.mezhvynskiy@gmail.com>
2018-12-24 14:31:49 +0200

    Quick timeout: misc updates

--- app/public/wp-content/themes/tory-theme/header.php
+++ app/public/wp-content/themes/tory-theme/header.php
@@ -13,12 +13,15 @@
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-           <ul>
-            <li><a href="<?php echo site_url('/about-me'); ?>">About Me</a></li>
-            <li <?php if(is_page('about-me') or wp_get_post_parent_id(0) == 17) echo 'class="current-menu-item' ?>><a href="#">Programs</a></li>
-            <li><a href="#">Events</a></li>
+          <ul>
+            <li <?php if(is_page('about-me') or wp_get_post_parent_id(0) == 17) echo
+              'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-me') ?>">About me</a></li>
+            <li><a href="#">Programs</a></li>
+            <li <?php if(get_post_type() == 'event') echo
+              'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event');  ?>">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li<?php if (get_post_type() == 'post') echo 'class ="current-menu-item" ' ?>><a href="<?php site_url('/blog'); ?>">Blog</a></li>
+            <li<?php if (get_post_type() == 'post') echo
+              'class ="current-menu-item"' ?>><a href="<?php site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
