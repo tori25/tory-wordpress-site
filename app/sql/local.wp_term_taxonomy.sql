38f9c4e5000c07275773044741a23bfe8ba5b830
tori25 <vera.mezhvynskiy@gmail.com>
2019-01-31 12:15:05 +0200

    removed node modules

--- app/sql/local.wp_term_taxonomy.sql
+++
@@ -1,9 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_term_taxonomy` VALUES
-(1,1,"category","",0,2),
-(2,2,"nav_menu","",0,2),
-(3,3,"nav_menu","",0,2),
-(4,4,"nav_menu","",0,2),
-(5,5,"category","Change this in wp-admin/categories/pop/edit/description",0,1);
