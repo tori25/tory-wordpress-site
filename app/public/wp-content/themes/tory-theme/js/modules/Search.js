fb8b3de97e7b2af35f4560524f37f15912f528b2
veram <vera.mezhvynskiy@gmail.coom>
2019-03-14 14:28:27 +0200

    new version

--- app/public/wp-content/themes/tory-theme/js/modules/Search.js
+++ app/public/wp-content/themes/tory-theme/js/modules/Search.js
@@ -43,22 +43,26 @@ class Search {
 	}
 
 	getResults() {
-		$.when(
-			$.getJSON(toryData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
-			$.getJSON(toryData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-		).then((results, pages) => {
-			let combinedResults = results[0].concat(pages[0]);
-			this.resultsDiv.html(
-				`<h2 class="search-overlay__section-title">General information</h2>
-					${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches this search.</p>'}
-					${combinedResults.map(item =>`<li><a href="${item.link}">
-						${item.title.rendered}</a>${item.type = 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
-					${combinedResults.length ? '</ul>' : ''}`);
-			this.isSpinnerVisible = false;
-		},
-			() => {
-			this.resultsDiv.html('<p>Unexpected error, please try again.</p>')
-			});
+		$.getJSON(toryData.root_url + '/wp-json/tory/v1/search?term=' + this.searchField.val()), (results) => {
+			this.resultsDiv.html(`
+				<div class="row">
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">General information</h2>
+					${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general information matches this search.</p>'}
+					${results.generalInfo.map(item =>`<li><a href="${	item.link}">${item.title.rendered}</a>${item.type = 'post' ?` by ${item.authorName}` : ''}</li>`).join('')}
+					${results.generalInfo.length ? '</ul>' : ''}
+					</div>
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">Genres</h2>
+						<h2 class="search-overlay__section-title">Singers</h2>
+					</div>
+					<div class="one-third">
+						<h2 class="search-overlay__section-title">Festivals</h2>
+						<h2 class="search-overlay__section-title">Events</h2>
+				  </div>
+				</div>
+			`);
+		});
 	}
 
 	openOverlay() {
